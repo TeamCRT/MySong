bdb2a8db9c901f734bf518bb7c47013a07b3a19f
Tim Ninan <tim.ninan@gmail.com>
2017-11-30 17:09:27 -0600

    added server folder and index.js file

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+
+// app.use(express.static(path.join(__dirname, '../client')));
+
+// app.get('/', function(req, res) {
+// 	console.log('home page requested');
+// 	//res.sendFile('../client/public/index.html');
+// })
+
+// app.listen(3001, function() {
+// 	console.log('listening on port 3001');
+// });
