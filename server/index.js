db7c5715620fe469a47a0f56ce1bdcb9cbb5a5ee
Ross Salge <rossnsalge@gmail.com>
2017-12-01 12:38:12 -0600

    in the process of ssetting up passport

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const mongoose = require('mongoose')
-require('dotenv').config({path:'../env.env'})
+const mongoose = require('mongoose');
+require('dotenv').config({path:'../env.env'});
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
 
 const app = express();
 
@@ -10,10 +13,19 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 console.log('Process/MongoDBUri', process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true})
-var db = mongoose.connection;
+//creates an express session that stores its session data into the mLab MongoDB endpoint
+//go to https://www.npmjs.com/package/connect-mongo for more information
+app.use(session({
+  secret: 'hratx30',
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: true,
+  saveUninitialized: false
+}));
+
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
+  console.log('Database connected!')
 });
 
 const port = process.env.PORT || 3001;
