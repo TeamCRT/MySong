a35edb8b565191c270f03fd4576b8089d82453fc
Ross Salge <rossnsalge@gmail.com>
2017-12-01 11:34:02 -0600

    begins setup for server and mongoose connections

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const mongoose = require('mongoose')
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/build')));
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true})
+
 const port = process.env.PORT || 3001;
 console.log('Running on ', port)
 app.listen(port);
