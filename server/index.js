fcc544d68bcb0315abdbff844ac291b25913a426
Ross Salge <rossnsalge@gmail.com>
2017-12-01 11:45:17 -0600

    adds dotenv as a dependencie

--- server/index.js
+++ server/index.js
@@ -6,7 +6,14 @@ const mongoose = require('mongoose')
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/build')));
+console.log('Process/MongoDBUri', process.env.MONGODB_URI);
+
 mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true})
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 const port = process.env.PORT || 3001;
 console.log('Running on ', port)
