a80040d8be414c41b1bcba4ed7e7abeb9b3ea887
Ross Salge <rossnsalge@gmail.com>
2017-12-01 16:27:01 -0600

    still in the process of setting up passport

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 require('dotenv').config({path:'../env.env'});
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 
 
 const app = express();
@@ -13,6 +16,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 console.log('Process/MongoDBUri', process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true})
+
 //creates an express session that stores its session data into the mLab MongoDB endpoint
 //go to https://www.npmjs.com/package/connect-mongo for more information
 app.use(session({
@@ -21,6 +25,7 @@ app.use(session({
   resave: true,
   saveUninitialized: false
 }));
+app.use(passport.initialize());
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
