9e2bc4c09d87a8d39e4c02f71054ccce2503e3c5
Ross Salge <rossnsalge@gmail.com>
2017-12-02 17:30:50 -0600

    Still setting up passport

---
+++ db/passport.js
@@ -0,0 +1,58 @@
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./model/user.js');
+
+module.exports = (passport) => {
+  // Local Signup Strategy
+  localSignup = passport.use('local-signup', new LocalStrategy(
+    { // http://www.passportjs.org/docs/login/ for more info
+      usernameField: 'username',// I don't think this field is necessary
+      passwordField: 'password',// I don't think this field is necessary
+      passReqToCallback: true,// req will be passed as the first argument to the verify callback
+    },
+    (username, password, done) => { // this is the verify callback mentioned in line 10
+      // Check to see if there is already a user with provided username
+      User.findOne({ username }, (err, user) => {
+        if (err) {
+          return done(err);
+        } else if (user) {
+          return done('username is already taken');
+        }
+        const newUser = new User();
+        // Set the user's local credentials
+        newUser.username = username;
+        newUser.password = newUser.generateHash(password);
+        // Save the user
+        newUser.save((err1) => {
+          if (err1) {
+            throw err1;
+          }
+          return done(null, newUser);
+        });
+      });
+    }
+  )),
+  // Local Login strategy
+  localLogin = passport.use('local-login', new LocalStrategy(
+    { // http://www.passportjs.org/docs/login/ for more info
+      usernameField: 'username',// I don't think this field is necessary
+      passwordField: 'password',// I don't think this field is necessary
+      passReqToCallback: true,// req will be passed as the first argument to the verify callback
+  },
+  function(req, username, password, done) {
+    User.findOne({ 'username': username }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      // If user is not found:
+      if (!user) {
+        return done('username not found.');
+      }
+      // If user is found but the provided password is incorrect:
+      if (!user.validPassword(password)) {
+        return done('loginMessage', 'Incorrect username/password.');
+      }
+      // If username and password are corret, return successfully
+      return done(null, user);
+    })
+  }))
+}
--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,34 @@
-//somthing in it
 const router = require('express').Router();
 const User = require('../../db/model/user.js');
+const helpers = require('./helpers.js');
 
 const passport = require('passport');/* http://www.passportjs.org/docs */
 
 router.get('/users', (req, res) => {
   User.find().exec((err, users) => {
-    res.send({users})
-  })
-})
+    res.send({ users });
+  });
+});
+
+var test = 0;
+
+// add new user
+/* Example POST data
+{
+  "username": "newuser",
+  "password": (hashed via bcrypt)
+}
+*/
+router.post('/signup', (req, res) => {
+  passport.authenticate('local-signup', (err, user) => {
+    if (err) {
+      res.status(401).send({ err });
+    } else {
+      req.session.user_id = user._id;
+      // user.email = req.body.email
+      // create a default list for the new user
+  })(req, res);
+});
+
 
 module.exports = router;
