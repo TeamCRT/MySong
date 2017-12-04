9320cad0d7d84dae1bc2964f6898c14028bc9057
Ross Salge <rossnsalge@gmail.com>
2017-12-04 11:34:07 -0600

    Establishes two basic routes from the front end to the serve

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../../db/model/user.js');
-const helpers = require('./helpers.js');
+// const helpers = require('./helpers.js');
 
 const passport = require('passport');/* http://www.passportjs.org/docs */
 
@@ -9,6 +9,20 @@ router.get('/users', (req, res) => {
     res.send({ users });
   });
 });
+
+router.get('/me', (req, res) => {
+  console.log('Getting me');
+  // var user_id = req.session.user_id;
+  // helpers.getUserById(user_id)
+  // .then((user) => {
+  //   res.send(user);
+  // })
+  // .catch((err) => {
+  //   res.send({});
+  // });
+  res.send({});
+});
+
 // add new user
 /* Example POST data
 {
@@ -17,15 +31,18 @@ router.get('/users', (req, res) => {
 }
 */
 router.post('/signup', (req, res) => {
+  console.log('Inside server/api post signup: ', req.body);
   passport.authenticate('local-signup', (err, user) => {
+    console.log('Passport authenticate user: ', user);
     if (err) {
       res.status(401).send({ err });
-    } else {
-      req.session.user_id = user._id;
-      // user.email = req.body.email
-      // create a default list for the new user
     }
+    res.send('This is a response from server/api/index.js .post /signup: ', user);
+    // req.session.user_id = user._id;
+    // user.email = req.body.email
+    // create a default list for the new user
   });
+  console.log('after passport authenticate');
 });
 
 
