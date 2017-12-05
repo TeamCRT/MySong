3bdf836399b5b68173a3491bd28c4cd92682f6a3
Ross Salge <rossnsalge@gmail.com>
2017-12-04 20:01:58 -0600

    Utilizes Passport strategy for spotify Oauth2, having issue with CORS due to create react boilerplate

--- db/passport.js
+++ db/passport.js
@@ -61,13 +61,14 @@ module.exports = (passport) => {
 >>>>>>> Local sign up is being passed through passport correctly
     },
   ));
-  passport.use(new SpotifyStrategy(
+  passport.use('spotify', new SpotifyStrategy(
     {
       clientID: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
       callbackURL: 'http://localhost:3001/auth/spotify/callback',
     },
     (accessToken, refreshToken, profile, done) => {
+      console.log('SpotifyStrategy check');
       User.findOrCreate(
         { spotifyId: profile.id },
         (err, user) => {
