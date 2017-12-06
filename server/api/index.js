ccd9933182f4c75b3024a1bf413897d0f59b6e6c
Ross Salge <rossnsalge@gmail.com>
2017-12-05 19:48:53 -0600

    clean up stage 2, remove cookie parser

--- server/api/index.js
+++ server/api/index.js
@@ -75,38 +75,6 @@ router.post(
   },
 );
 
-// var generateRandomString = function(length) {
-//   var text = '';
-//   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//
-//   for (var i = 0; i < length; i++) {
-//     text += possible.charAt(Math.floor(Math.random() * possible.length));
-//   }
-//   return text;
-// };
-//
-// var stateKey = 'spotify_auth_state';
-// var redirect_uri = 'http://localhost:3001/callback'; // Your redirect uri
-
-
-// router.get('/login', function(req, res) {
-//
-//   var state = generateRandomString(16);
-//   res.cookie(stateKey, state);
-//
-//   // your application requests authorization
-//   var scope = 'user-read-private user-read-email';
-//   res.redirect('https://accounts.spotify.com/authorize?' +
-//     querystring.stringify({
-//       response_type: 'code',
-//       client_id: process.env.SPOTIFY_CLIENT_ID,
-//       scope: 'user-read-private user-read-email',
-//       redirect_uri: 'http://localhost:3001/callback',
-//       state: '013747suhqlxJA73'
-//     }));
-// });
-
-
 router.get('/callback', (req, res) => {
   console.log('inside of api/callback')
 });
