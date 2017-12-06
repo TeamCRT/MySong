629f5f258297a59240117e2ad8e4ed2679ff3bcd
Ross Salge <rossnsalge@gmail.com>
2017-12-05 20:31:01 -0600

    clean up stage 4, removed much from server/index.js, added comments

--- server/api/index.js
+++ server/api/index.js
@@ -20,18 +20,15 @@ router.get('/me', (req, res) => {
 // spotify OAuth strategy
 router.get(
   '/auth/spotify',
-  passport.authenticate('spotify',
+  passport.authenticate(
+    'spotify',
     {
       scope: ['user-read-email', 'user-read-private'],
-      showDialog: false,
+      showDialog: true,
       successRedirect: '/',
       failureRedirect: '/login',
     },
   ),
-  (req, res) => {
-    // The request will be redirected to spotify for authentication, so this
-    // function will not be called.
-  },
 );
 // spotify OAuth callback for authorization process
 router.get(
@@ -50,33 +47,5 @@ router.get(
     res.redirect('/');
   },
 );
-
-// add new user
-/* Example POST data
-{
-  "username": "newuser",
-  "password": (hashed via bcrypt)
-}
-*/
-router.post(
-  '/signup',
-  (req, res, next) => {
-    passport.authenticate(
-      'local-signup',
-      (err, user) => {
-        if (err) {
-          return next(err);
-        }
-        console.log('successful passport authenticate: ', user);
-        res.send({ user });
-        return true;
-      },
-    )(req, res, next);
-  },
-);
-
-router.get('/callback', (req, res) => {
-  console.log('inside of api/callback')
-});
 
 module.exports = router;
