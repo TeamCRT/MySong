725df0d9132f842b2742c36c7dfa868d8edb864c
Carter Duncan <dukeblue2017@gmail.com>
2017-12-06 12:22:59 -0600

    Fix lint issues in React components

--- client/src/components/Main/CurrentPlaylistSong.jsx
+++ client/src/components/Main/CurrentPlaylistSong.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { Grid, Button, Divider } from 'semantic-ui-react'
+import React from 'react';
+import { Grid, Button } from 'semantic-ui-react';
 
 const CurrentPlaylistSong = () => (
 
- <Grid.Column color='red' style={{ width: 400 }}>
- 
-  <Button.Group content='CurrentPlaylistSong'>
-    <Button icon='play' />
-    <Button icon='pause' />
-    <Button icon='shuffle' />
-  </Button.Group>
+  <Grid.Column color="red" style={{ width: 400 }}>
+    <Button.Group content="CurrentPlaylistSong">
+      <Button icon="play" />
+      <Button icon="pause" />
+      <Button icon="shuffle" />
+    </Button.Group>
 
- </Grid.Column>
-)
+  </Grid.Column>
+);
 
- export default CurrentPlaylistSong
+export default CurrentPlaylistSong;
--- client/src/components/MyCurrentSong/MyCurrentSongContainer.jsx
+++ client/src/components/MyCurrentSong/MyCurrentSongContainer.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { Button, Segment } from 'semantic-ui-react'
+import React from 'react';
+import { Button, Segment } from 'semantic-ui-react';
 
 const MyCurrentSongContainer = () => (
   <div>
-    <Button attached='top'>MySong this week</Button>
-    <Segment attached>
-    </Segment>
+    <Button attached="top">MySong this week</Button>
+    <Segment attached />
   </div>
-)
+);
 
-export default MyCurrentSongContainer
+export default MyCurrentSongContainer;
