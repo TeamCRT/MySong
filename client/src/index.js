10b92e7e7ab4ba53d470fd87b3a19693802e4a2e
Tim Ninan <tim.ninan@gmail.com>
2017-12-07 02:15:14 -0600

    added stylistic changes to landing page, added new favicon

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import "typeface-roboto";
+import "typeface-bungee";
+import "typeface-comfortaa";
+import "typeface-rubik";
+import "typeface-oxygen";
+import "typeface-signika";
+import "typeface-heebo";
+import "typeface-baloo";
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
