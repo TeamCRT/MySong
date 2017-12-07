cb2bb85ab71afe3cf5a38accd9692069d4b44080
Carter Duncan <dukeblue2017@gmail.com>
2017-12-07 17:21:33 -0600

    Move Following Component files inside of homepage
    Move main into HomePage
    
    Move MyCurrentSong inside of HomePage
    
    Move Playlist and PlaylistsContainer
    
    Change reference to PlaylistContainer in HomePage
    
    Rename PlaylistsContainer to PlaylistContainer
    
    Rename Playlist to PlaylistEntry
    
    Change a reference to Playlist to PlaylistEntry

--- client/src/components/HomePage/Playlists/PlaylistEntry.jsx
+++ client/src/components/HomePage/Playlists/PlaylistEntry.jsx
@@ -4,6 +4,6 @@ import { Button } from 'semantic-ui-react';
 // props received here from PlaylistContainer are:
 // title and key (the spotify URI)
 
-const Playlist = props => <Button>{props.title}</Button>;
+const PlaylistEntry = props => <Button>{props.title}</Button>;
 
-export default Playlist;
+export default PlaylistEntry;
