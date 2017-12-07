0c0507edbe8eaebfdc7a38816b054ba133fa119d
Tim Ninan <tim.ninan@gmail.com>
2017-12-07 02:15:14 -0600

    added stylistic changes to landing page, added new favicon

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -13,6 +13,9 @@ import {
   Visibility,
 } from 'semantic-ui-react'
 import piano from './playing-piano-wallpaper.jpg'
+import guitar from './guitar.jpg'
+import phone from './phone.jpg'
+import headphones from './headphones.jpg'
 
 const FixedMenu = () => (
   <Menu fixed='top' size='large'>
@@ -49,20 +52,20 @@ export default class LandingPage extends Component {
           onBottomVisible={this.hideFixedMenu}
           once={false}
         >
-          <Segment
+          <Segment 
             inverted
             textAlign='center'
-            style={{ minHeight: 700, padding: '1em 0em' }}
+            style={{ minHeight: 700, padding: '1em 0em', backgroundImage:`url(${headphones})`, backgroundSize:'cover' }}
             vertical
           >
-            <Container>
+            <Container >
               <Menu inverted pointing secondary size='large'>
                 <Menu.Item as='a' active>Home</Menu.Item>
                 <Menu.Item as='a'>About</Menu.Item>
                 <Menu.Item as='a'>Contact Us</Menu.Item>
                 <Menu.Item position='right'>
-                  <Button as='a' inverted onClick={this.onClick}>Log in with Spotify
-                    <Icon size='large' color='green' name='spotify' />
+                  <Button as='a' inverted style={{fontSize:'15px'}} onClick={this.onClick}>Log in with Spotify
+                    <Icon style={{marginLeft:'0.5em', marginBottom:'5px'}} size='big' color='green' name='spotify' />
                   </Button>
                 </Menu.Item>
               </Menu>
@@ -73,25 +76,25 @@ export default class LandingPage extends Component {
                 as='h1'
                 content='mysong'
                 inverted
-                style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+                style={{ fontFamily:'Bungee', fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '.75em' }}
               />
               <Header
                 as='h2'
-                content='share your favorite song of the week. '
+                content='Share your favorite song of the week. '
                 inverted
-                style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+                style={{  fontFamily:'Rubik', fontSize: '1.75em', fontWeight: 'normal' }}
               />
               <Header
                 as='h2'
-                content='listen to favorite songs from the people in your life.'
+                content='Listen to favorite songs from the people in your life.'
                 inverted
-                style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+                style={{ fontFamily:'Rubik', fontSize: '1.75em', fontWeight: 'normal' }}
               />
               <Header
                 as='h2'
-                content=' fully integrated with Spotify.'
+                content=' Fully integrated with Spotify.'
                 inverted
-                style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+                style={{ fontFamily:'Rubik', fontSize: '1.75em', fontWeight: 'normal' }}
               />
 
             </Container>
@@ -102,13 +105,13 @@ export default class LandingPage extends Component {
           <Grid container stackable verticalAlign='middle'>
             <Grid.Row>
               <Grid.Column width={8}>
-                <Header as='h3' style={{ fontSize: '2em' }}>Share Your Favorite Song</Header>
-                <p style={{ fontSize: '1.33em' }}>
+                <Header as='h3' style={{ fontFamily: 'Baloo', fontSize: '2em' }}>Share Your Favorite Song</Header>
+                <p style={{ fontFamily: 'Oxygen', fontSize: '1.33em' }}>
                   Every week, you pick a MySong to share. We believe in the psychology of constraints - having to select just one song to share makes the song you share more meaningful.
                 </p>
-                <Header as='h3' style={{ fontSize: '2em' }}>Listen To Favorite Songs</Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  Create playlists of MySongs from the people in your life. Engage in deeper interaction and dialogue with those you know, and discover new music.
+                <Header as='h3' style={{ fontFamily: 'Baloo', fontSize: '2em' }}>Listen To Favorite Songs</Header>
+                <p style={{ fontFamily: 'Oxygen', fontSize: '1.33em' }}>
+                  Create playlists of MySongs from the people in your life. Engage in deeper interaction and dialogue with those you know, and discover new music. 
                 </p>
               </Grid.Column>
               <Grid.Column floated='right' width={6}>
