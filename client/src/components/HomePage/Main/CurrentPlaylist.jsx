7c4098dbcd71c086c7a0261726d2c44aaa217c45
Carter Duncan <dukeblue2017@gmail.com>
2017-12-11 12:24:08 -0600

    Refactor CurrentPlaylist component to be stateful

--- client/src/components/HomePage/Main/CurrentPlaylist.jsx
+++ client/src/components/HomePage/Main/CurrentPlaylist.jsx
@@ -1,31 +1,52 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import axios from 'axios';
 import CurrentPlaylistSong from './CurrentPlaylistSong';
 
-const CurrentPlaylist = props => (
-  <div>
-    <Grid
-      centered
-      columns={3}
-      padded
-      stackable
-      stretched
-      style={{ margin: '5.0em' }}
-      textAlign="center"
-    >
-      <Grid.Column color="grey" style={{ width: 400 }}>
-         Current Playlist: {props.currentPlaylistObj.title}
-      </Grid.Column>
-      <CurrentPlaylistSong />
-    </Grid>
-    <iframe
-      title="bottomPlayerIframe"
-      src={`https://open.spotify.com/embed?uri=${props.currentPlaylistObj.playlistURI}`}
-      frameBorder="0"
-      height="400"
-      width="100%"
-    />
-  </div>
-);
+class CurrentPlaylist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tracks: null,
+    };
+
+    axios.get('/api/aplaylist')
+      .then((response) => {
+        console.log('response from aplaylist', response);
+        this.setState({ tracks: response.data[0].playlists });
+        return response;
+      })
+      .catch(err => err);
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid
+          centered
+          columns={3}
+          padded
+          stackable
+          stretched
+          style={{ margin: '5.0em' }}
+          textAlign="center"
+        >
+          <Grid.Column color="grey" style={{ width: 400 }}>
+             Current Playlist: {this.props.currentPlaylistObj.title}
+          </Grid.Column>
+          <CurrentPlaylistSong />
+        </Grid>
+        <iframe
+          title="bottomPlayerIframe"
+          src={`https://open.spotify.com/embed?uri=${this.props.currentPlaylistObj.playlistURI}`}
+          frameBorder="0"
+          height="400"
+          width="100%"
+        />
+      </div>
+    )
+  }
+
+};
 
 export default CurrentPlaylist;
