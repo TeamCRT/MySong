ced48b82e8de4b1befab17bfe90fcc6932a1fef0
Carter Duncan <dukeblue2017@gmail.com>
2017-12-11 15:22:23 -0600

    Refactor MyCurrentSongContainer, no longer a class

--- client/src/components/HomePage/MyCurrentSong/MyCurrentSongContainer.jsx
+++ client/src/components/HomePage/MyCurrentSong/MyCurrentSongContainer.jsx
@@ -2,25 +2,20 @@ import React from 'react';
 import { Segment, Header } from 'semantic-ui-react';
 import MySongModal from './MySongModal';
 
-class MyCurrentSongContainer extends React.Component {
- constructor(props) {
-   super(props);
- }
-  render() {
-    return (
-  	  <div>
-        <Segment attached="top">
-	        <Header as="h1" style={{ textAlign: 'center' }}>
-	          Current My Song is : {this.props.currentMySong.trackSummary}
-	          <div style={{fontSize:'15px'}}>
-	            Note: {this.props.currentMySong.note}
-	          </div>
-	        </Header>
-    	    <MySongModal spotifyId={this.props.spotifyId} spotifyToken={this.props.spotifyToken} onMySongChange={this.props.onMySongChange}/>
-        </Segment>
-      </div>
-  	)
-	}
-
-}
+const MyCurrentSongContainer = props => (
+  <div>
+    <Segment attached="top">
+      <Header as="h1" style={{ textAlign: 'center' }}>
+        Current My Song is : {props.currentMySong.trackSummary}
+        <div style={{ fontSize: '15px' }}>
+          Note: {props.currentMySong.note}
+        </div>
+      </Header>
+      <MySongModal
+        spotifyId={props.spotifyId}
+        spotifyToken={props.spotifyToken}
+        onMySongChange={props.onMySongChange} />
+    </Segment>
+  </div>
+);
 export default MyCurrentSongContainer;
