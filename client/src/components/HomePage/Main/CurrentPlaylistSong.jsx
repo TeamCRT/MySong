8505ebbe5c2a0e871c73a4970775d9d0a9f80a09
Carter Duncan <dukeblue2017@gmail.com>
2017-12-12 14:03:32 -0600

    Style the current playlist and make title visible

--- client/src/components/HomePage/Main/CurrentPlaylistSong.jsx
+++ client/src/components/HomePage/Main/CurrentPlaylistSong.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Grid, Button } from 'semantic-ui-react';
 
-const CurrentPlaylistSong = (props) => {
-  return (
-    <Grid.Column color="red" style={{ width: '100%' }}>
-      <Button.Group>
-        <Button icon="play" />
-        <Button icon="pause" />
-        <Button>{`${props.user}'s song: ${props.trackObj.trackName} by ${props.trackObj.trackArtist} on the album ${props.trackObj.trackAlbum}. Note: ${props.trackObj.note}`}</Button>
-      </Button.Group>
-
+const CurrentPlaylistSong = props => (
+  <Grid>
+    <Grid.Column color="red" style={{ width: '15%', borderRadius: '20px 0px 0px 20px', marginBottom: '5px'}}>
+      <iframe
+        title={`${props.user}Iframe`}
+        src={`https://open.spotify.com/embed?uri=spotify:track:${props.trackObj.trackID}`}
+        frameBorder="0"
+        height="80"
+        width="80"
+      />
     </Grid.Column>
-  );
-};
+    <Grid.Column color="red" style={{ width: '85%', borderRadius: '0px 20px 20px 0px', marginBottom: '5px' }}>
+      <Button.Group>
+        <Button>
+          <h3>{`${props.user}'s song: ${props.trackObj.trackName}`}</h3>
+          <div>{`Artist: ${props.trackObj.trackArtist}`}</div>
+          <div>{`Album: ${props.trackObj.trackAlbum}`}</div>
+          <div>{`Note: ${props.trackObj.note}`}</div>
+        </Button>
+      </Button.Group>
+    </Grid.Column>
+  </Grid>
+);
 
 export default CurrentPlaylistSong;
