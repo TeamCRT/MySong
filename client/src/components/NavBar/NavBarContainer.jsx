7e91be1d76161b06b2f0252fa00b32087e219bd9
Ross Salge <rossnsalge@gmail.com>
2017-12-12 10:29:40 -0600

    Deletes token on client side, but comes back with refresh, PUMR

--- client/src/components/NavBar/NavBarContainer.jsx
+++ client/src/components/NavBar/NavBarContainer.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { Menu, Header } from 'semantic-ui-react';
+import { Button, Menu, Header } from 'semantic-ui-react';
 import Search from './Search.jsx'
 import './NavBarContainer.css';
 
 
-const NavBarContainer = props => (
-  <Menu>
-    <Menu.Item>
-      <Search options={props.options} />
-    </Menu.Item>
-    <Header size="large" color="red" style={{fontFamily:'Bungee', fontSize:'50px', marginLeft:'11em'}} floated="right">mysong.io </Header>
-    <Menu.Item name={props.username} position="right" />
-  </Menu>
-);
+class NavBarContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
+  handleLogOut() {
+    window.localStorage.removeItem('token');
+  }
+  render() {
+    return (
+      <Menu>
+        <Menu.Item>
+          <Search options={this.props.options} />
+        </Menu.Item>
+        <Header size="large" color="red" style={{fontFamily:'Bungee', fontSize:'50px', marginLeft:'11em'}} floated="right">mysong.io </Header>
+        <Menu.Item name={this.props.username} position="right" />
+        <Menu.Item>
+          <Button onClick={this.handleLogOut}> Log Out </Button>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+};
 
 export default NavBarContainer;
