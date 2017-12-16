566a2fc499e42a54aca7c773c62af954428913bd
Carter Duncan <dukeblue2017@gmail.com>
2017-12-16 16:37:46 -0600

    Render PlaylistContainer even if there are 0 playlists

--- client/src/components/HomePage/Playlists/PlaylistContainer.jsx
+++ client/src/components/HomePage/Playlists/PlaylistContainer.jsx
@@ -1,32 +1,14 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { Button } from 'semantic-ui-react';
-import axios from 'axios';
 import PlaylistEntry from './PlaylistEntry';
 import CreatePlaylistModal from './CreatePlaylistModal';
 
 class PlaylistContainer extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   playlists: [],
-    // };
-
-    // axios.get(`/api/playlists?spotifyUserID=${props.spotifyId}`)
-    //   .then((response) => {
-    //     this.setState({ playlists: response.data[0].playlists });
-    //     return response;
-    //   })
-    //   .catch(err => err);
+    this.mapFunction = this.mapFunction.bind(this);
   }
-
-  // updatePlaylists() {
-  //   axios.get(`/api/playlists?spotifyUserID=${this.props.spotifyId}`)
-  //     .then((response) => {
-  //       this.setState({ playlists: response.data[0].playlists });
-  //       return response;
-  //     })
-  //     .catch(err => err);
-  // }
 
   mapFunction(playlistObj) {
     return (
@@ -43,21 +25,19 @@ class PlaylistContainer extends React.Component {
   render() {
     return (
       <div>
-        {this.props.playlists[0] &&
-          <div>
-            <Button.Group vertical style={{ width: '100%' }}>
-              <Button disabled >My Playlists</Button>
-              {this.props.playlists.map(this.mapFunction.bind(this))}
-              <CreatePlaylistModal
-                spotifyId={this.props.spotifyId}
-                color="red"
-                following={this.props.following}
-                updatePlaylists={this.props.updatePlaylists}
-                playlists = {this.props.playlists}
-              /> 
-            </Button.Group>
-          </div>
-        }
+
+        <div>
+          <Button.Group vertical style={{ width: '100%' }}>
+            <Button disabled >My Playlists</Button>
+            {this.props.playlists.map(this.mapFunction)}
+            <CreatePlaylistModal
+              spotifyId={this.props.spotifyId}
+              color="red"
+              following={this.props.following}
+              updatePlaylists={this.props.updatePlaylists}
+            />
+          </Button.Group>
+        </div>
       </div>
     );
   }
