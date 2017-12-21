555dfc4fbc348322d7b361b346f946ba66682e9c
Tim Ninan <tim.ninan@gmail.com>
2017-12-21 01:10:36 -0600

    got alignment just right for the top black bars

--- client/src/components/HomePage/MyCurrentSong/SearchResults.js
+++ client/src/components/HomePage/MyCurrentSong/SearchResults.js
@@ -12,10 +12,10 @@ class SearchResults extends React.Component {
     return (
 
            <div id="search-results" style={{backgroundColor: '#575159', display: 'flex', flexDirection: 'column', width: '50%', height: '100%', flexGrow: '1'}} >
-             <div id="searchresult1" style={{backgroundColor: 'black', display: 'flex', flexDirection: 'row', width: '100%', minHeight: '40.23px', maxHeight: '43.32px'}}>
-               <Label id="track-header" style={{width: '60%', maxWidth: '60%', minWidth: '60%', height: '100%', borderRadius: '0px', fontSize: '20px', textAlign: 'center', color: 'white', backgroundColor: 'black'}}>Track</Label>
-               <Label id="artist-header" style={{width: '20%', maxWidth: '20%', minWidth: '20%', height: '100%', borderRadius: '0px', fontSize: '20px', textAlign: 'center', color: 'white', backgroundColor: 'black'}}>Artist</Label>
-               <Label id="album-header" style={{width: '20%', maxWidth: '20%', minWidth: '20%', height: '100%', borderRadius: '0px', fontSize: '20px', textAlign: 'center', color: 'white', backgroundColor: 'black'}}>Album</Label>
+             <div id="searchresult1" style={{backgroundColor: 'black', display: 'flex', flexDirection: 'row', width: '100%', minHeight: '43.32px', maxHeight: '43.32px'}}>
+               <div id="track-header" style={{width: '60%', maxWidth: '60%', minWidth: '60%', height: '100%', borderRadius: '0px', fontSize: '20px', textAlign: 'center', color: 'white', backgroundColor: 'black', padding: '.5em .5em'}}>Track</div>
+               <div id="artist-header" style={{width: '20%', maxWidth: '20%', minWidth: '20%', height: '100%', borderRadius: '0px', fontSize: '20px', textAlign: 'center', color: 'white', backgroundColor: 'black', padding: '.5em .5em'}}>Artist</div>
+               <div id="album-header" style={{width: '20%', maxWidth: '20%', minWidth: '20%', height: '100%', borderRadius: '0px', fontSize: '20px', textAlign: 'center', color: 'white', backgroundColor: 'black', padding: '.5em .5em'}}>Album</div>
              </div>
              {this.props.searchResults.map((result, index) => (<SearchResult result={result} />))}  
            </div>
