eecc1e33dc79456a26a11fa9681d130be35f2302
Carter Duncan <dukeblue2017@gmail.com>
2017-12-20 18:54:43 -0600

    Restore CurrentPlaylistSong from before rebase

--- client/src/components/HomePage/Main/CurrentPlaylistSong.jsx
+++ client/src/components/HomePage/Main/CurrentPlaylistSong.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const CurrentPlaylistSong = props => (
   <div className="current-playlist-song">
+    <h3 className="userName">{props.user}</h3>
     <div className="artwork">
       <iframe
         title={`${props.user}Iframe`}
@@ -13,10 +14,9 @@ const CurrentPlaylistSong = props => (
     </div>
     <div className="song-info">
       <div>
-        <h3>{`${props.user}'s song: ${props.trackObj.trackName}`}</h3>
+        <h3>{props.trackObj.trackName}</h3>
         <div>{`Artist: ${props.trackObj.trackArtist}`}</div>
         <div>{`Album: ${props.trackObj.trackAlbum}`}</div>
-        <div>{`Note: ${props.trackObj.note}`}</div>
       </div>
     </div>
     <div className="note">{props.trackObj.note}</div>
